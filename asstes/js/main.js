84415006b9b14f7a9af423adfa6a91f9cd733699
JEaLiFe <83034287+jealife@users.noreply.github.com>
2023-07-02 18:39:33 +0100

    Add files via upload

---
+++ asstes/js/main.js
@@ -0,0 +1,15 @@
+
+
+// Swiper js 
+let swiperportfolio = new Swiper(".portfolio", {
+    // spaceBetween: 24,
+    loop: true,
+    grabCursor: true,
+    // slidesPerView: 2,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    // 
+
+});
